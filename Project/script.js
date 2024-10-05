3b12e02788051e473ea05f4e6949fef71bf1f6a2
mirzaahtesham2003 <mirzaahtesham2003@gmail.com>
2024-10-05 14:35:15 +0530

    express

--- Project/script.js
+++ Project/script.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const app = express();
+
+const express = require('express')
+const app = express()
 
 app.get('/', function (req, res) {
-    res.send('Hello');
+  res.send('Hello World')
 })
 
-app.listen(3000);
+app.listen(3000)
