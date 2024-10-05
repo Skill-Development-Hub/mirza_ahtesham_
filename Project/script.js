b59f7899a31c77032cec7bc6d0083ba9e2d39003
mirzaahtesham2003 <mirzaahtesham2003@gmail.com>
2024-10-05 23:13:53 +0530

    EndOfDay

--- Project/script.js
+++ Project/script.js
@@ -2,8 +2,24 @@
 const express = require('express')
 const app = express()
 
+app.set("view engine", "ejs");  
+
+app.use(express.static('./public'));
+
+app.use(function (req, res, next){
+    console.log('salam');
+    next();
+});
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.render("index");
 })
+
+app.get('/contact', function(req, res){
+    res.render("contact");
+})
+
+
+
 
 app.listen(3000)
